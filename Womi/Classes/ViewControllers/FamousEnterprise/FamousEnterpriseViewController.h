ce88c21e42c6dc85c03f038f4ac653359f6877f1
NaNa <zhao_na_na@126.com>
2015-01-30 11:35:50 +0800

    添加几个丢失的类

---
+++ Womi/Classes/ViewControllers/FamousEnterprise/FamousEnterpriseViewController.h
@@ -0,0 +1,13 @@
+//
+//  FamousEnterpriseViewController.h
+//  Womi
+//
+//  Created by NaNa on 15/1/30.
+//  Copyright (c) 2015年 NaNa. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface FamousEnterpriseViewController : BaseViewController
+
+@end
---
+++ Womi/Classes/ViewControllers/LabelManage/LabelManageViewController.h
@@ -0,0 +1,13 @@
+//
+//  LabelManageViewController.h
+//  Womi
+//
+//  Created by NaNa on 15/1/30.
+//  Copyright (c) 2015年 NaNa. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface LabelManageViewController : BaseViewController
+
+@end
